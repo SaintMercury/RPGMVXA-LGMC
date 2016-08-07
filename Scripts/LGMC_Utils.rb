8cdc9734f800d84b8a1b8e3b5ecf174f3eab23e9
Max Henzerling <maxhenzerling@gmail.com>
2016-08-06 23:49:49 -0700

    Max: Added more slimy skills

--- Scripts/LGMC_Utils.rb
+++ Scripts/LGMC_Utils.rb
@@ -1,13 +1,37 @@
-=begin
-This is me poopin around with scripts
-
-=end
 module LGMC_Utils
-  def getMonsterElement(monster)
+  def self.assimilate
+    monsterElements = []
+    monsters = $game_troop.alive_members
+    for monsterInTroop in monsters
+      monster = $data_enemies[monsterInTroop.enemy_id]
+      element = getMonsterElement(monster)
+      monsterElements.push(element)
+    end
+    element = findMostCommonElement(monsterElements)
+    return element
+  end
+
+  def self.getMonsterElement(monster)
     return parseForContent(monster.note, "element")
   end
 
-  def parseForContent(note, tag)
+  def self.findMostCommonElement(array)
+    max = 0
+    tallies = Hash.new(0)
+    mostCommon = ""
+
+    for element in array
+      tallies[element] += 1
+      if tallies[element] > max
+        max = tallies[element]
+        mostCommon = element
+      end
+    end
+
+    return mostCommon
+  end
+
+  def self.parseForContent(note, tag)
     #The following regex will match for a tag within a string,
     #and return the contents of that tag
     #Tag syntax
@@ -17,4 +41,8 @@ module LGMC_Utils
     content = note.scan(regex)[0][0]
     return content
   end
+end
+
+class Game_Interpreter
+  include LGMC_Utils
 end
